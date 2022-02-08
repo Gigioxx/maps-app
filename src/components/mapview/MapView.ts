01c427c6b17d4cdf2ad4dfded27949424b0b2a87
Gigiox <guillermo.casanova.b@gmail.com>
2022-02-08 17:25:16 -0300

    Show map when having user Location

--- src/components/mapview/MapView.ts
+++ src/components/mapview/MapView.ts
@@ -1,5 +1,6 @@
-import { defineComponent, onMounted, ref } from 'vue'
+import { defineComponent, onMounted, ref, watch } from 'vue'
 import { usePlacesStore } from '@/composables/usePlacesStore';
+import Mapboxgl from 'mapbox-gl';
 
 export default defineComponent({
     name: 'MapView',
@@ -9,9 +10,28 @@ export default defineComponent({
 
         const { userLocation, isUserLocationReady } = usePlacesStore();
 
+        const initMap = async() => {
+            if ( !mapElement.value ) throw new Error('Div element does not exist');
+            if ( !userLocation.value ) throw new Error('user location does not exist');
+
+            await Promise.resolve();
+            
+            const map = new Mapboxgl.Map({
+                container: mapElement.value, // container ID
+                style: 'mapbox://styles/mapbox/streets-v11', // style URL
+                center: userLocation.value,
+                zoom: 15 // starting zoom
+            });
+        }
+
         onMounted(() => {
-           console.log( mapElement.value ) 
+           if ( isUserLocationReady.value )
+            return initMap();
         });
+
+        watch( isUserLocationReady, ( newVal ) => {
+            if ( userLocation.value ) initMap();
+        })
 
         return {
             isUserLocationReady,
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import mapboxgl from 'mapbox-gl';
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ2lnaW94eCIsImEiOiJja3plaXg3NDAyZ3hyMnVwcXZscGpwYml2In0.WX8De1C_vIrQRvErolbqwQ';
+
 if ( !navigator.geolocation ) {
     alert('Tu navegador no soporta el GeoLocation')
     throw new Error('Tu navegador no soporta el GeoLocation')
