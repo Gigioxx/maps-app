80d979120d77833a16923f82b15302e7a6f6c892
Gigiox <guillermo.casanova.b@gmail.com>
2022-02-10 12:21:23 -0300

    Save places in vuex Store

--- src/store/places/actions.ts
+++ src/store/places/actions.ts
@@ -2,7 +2,7 @@ import { ActionTree } from 'vuex';
 import { PlacesState } from './state';
 import { StateInterface } from '../index';
 import { searchApi } from '@/apis';
-import { PlacesResponse } from '@/interfaces/places';
+import { Feature, PlacesResponse } from '@/interfaces/places';
 
 const actions: ActionTree<PlacesState, StateInterface> = {
     getInitialLocation({ commit }) {
@@ -17,15 +17,28 @@ const actions: ActionTree<PlacesState, StateInterface> = {
     },
 
     // Todo: Colocar el valor de retorno
-    async searchPlacesByTerm({ commit, state }, query: string) {
+    async searchPlacesByTerm({ commit, state }, query: string): Promise<Feature[]> {
+
+        if ( query.length === 0 ) {
+            commit('setPlaces', []);
+            return [];
+        }
+
+        if ( !state.userLocation ){
+            throw new Error('No hay ubicación del usuario');
+        }
+
+        commit('setIsLoadingPlaces');
         
         const resp = await searchApi.get<PlacesResponse>(`/${ query }.json`, {
             params: {
                 proximity: state.userLocation?.join(',')
             }
-        })
+        });
 
-        console.log(resp.data.features);
+        commit('setPlaces', resp.data.features );
+
+        return resp.data.features;
 
     }
 }
