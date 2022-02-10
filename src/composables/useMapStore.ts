2f57ba5d30e5f89b3d2a92ebd7e04f2088f7be4e
Gigiox <guillermo.casanova.b@gmail.com>
2022-02-10 14:58:48 -0300

    Setting markers on searchResult places

--- src/composables/useMapStore.ts
+++ src/composables/useMapStore.ts
@@ -3,6 +3,7 @@ import { useStore } from 'vuex';
 import { StateInterface } from '@/store';
 
 import Mapboxgl from 'mapbox-gl';
+import { Feature } from '@/interfaces/places';
 
 
 export const useMapStore = () => {
@@ -19,6 +20,7 @@ export const useMapStore = () => {
 
         // Mutations
         setMap: ( map: Mapboxgl.Map ) => store.commit('map/setMap', map ),
+        setPlaceMarkers: ( places: Feature[] ) => store.commit('map/setPlaceMarkers', places ),
 
         // Actions
     }
