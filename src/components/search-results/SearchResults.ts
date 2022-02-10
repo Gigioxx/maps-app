bbbb35b62019b6a913db558393b8663e5e76060f
Gigiox <guillermo.casanova.b@gmail.com>
2022-02-10 15:31:24 -0300

    Get information about route between two places

--- src/components/search-results/SearchResults.ts
+++ src/components/search-results/SearchResults.ts
@@ -6,8 +6,8 @@ export default defineComponent({
     name: 'SearchResults',
     setup() {
 
-        const { isLoadingPlaces, places } = usePlacesStore();
-        const { map, setPlaceMarkers } = useMapStore();
+        const { isLoadingPlaces, places, userLocation } = usePlacesStore();
+        const { map, setPlaceMarkers, getRouteBetweenPoints } = useMapStore();
 
         const activePlace = ref('');
 
@@ -27,7 +27,19 @@ export default defineComponent({
                 map.value?.flyTo({
                     center: [ lng, lat ],
                     zoom: 14
-                })
+                });
+            },
+
+            getRouteDirections: ( place: Feature ) => {
+                if ( !userLocation.value ) return;
+
+                const [ lng,lat ] = place.center;
+                const[ startLng, startLat ] = userLocation.value;
+
+                const start: [number, number] = [startLng, startLat];
+                const end: [number, number] = [lng, lat];
+                
+                getRouteBetweenPoints( start, end );
             }
         }
     } 
--- src/composables/useMapStore.ts
+++ src/composables/useMapStore.ts
@@ -4,6 +4,7 @@ import { StateInterface } from '@/store';
 
 import Mapboxgl from 'mapbox-gl';
 import { Feature } from '@/interfaces/places';
+import { LngLat } from '@/store/map/actions'
 
 
 export const useMapStore = () => {
@@ -23,5 +24,6 @@ export const useMapStore = () => {
         setPlaceMarkers: ( places: Feature[] ) => store.commit('map/setPlaceMarkers', places ),
 
         // Actions
+        getRouteBetweenPoints: ( start: LngLat, end: LngLat ) => store.dispatch('map/getRouteBetweenPoints', { start, end }),
     }
 }
